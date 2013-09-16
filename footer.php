bd8af5a2d1fa1529ac803b71b9e6ce5926908cc7
Bank <Bank@Bank-PC>
2013-09-16 13:32:07 +0700

    add  header footer file php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="copyright">
+        &copy; 2012 <strong class="footer-logo">Pet clinic</strong>
+        <div><a rel="nofollow" href="http://www.templatemonster.com">Website Template</a> by TemplateMonster.com</div>
+		<div>More <a href="http://www.websitetemplatesonline.com" title="WTO - website templates and Flash templates" target="_blank">Free Web Templates</a> at WTO.</div>
+    </div>
+    <ul class="social-list">
+    	<li><a href="#"><img src="images/social-link-1.jpg" alt=""></a></li>
+        <li><a href="#"><img src="images/social-link-2.jpg" alt=""></a></li>
+        <li><a href="#"><img src="images/social-link-3.jpg" alt=""></a></li>
+        <li><a href="#"><img src="images/social-link-4.jpg" alt=""></a></li>
+    </ul>
+</footer>
---
+++ hearder.php
@@ -0,0 +1,43 @@
+    <div class="inner">
+        <h1 class="logo"><a href="home.html">Pet clinic</a></h1>
+        <div class="fright">
+            <div class="header-meta">
+                <form id="search-form" method="post">
+                    <input type="text" onblur="if(this.value==''){this.value='search'}" onfocus="if(this.value=='search'){this.value=''}" value="search" name="search">
+                    <a class="search-form-submit" onclick="document.getElementById('search-form').submit()"></a>
+                </form>
+                <div class="col-elem">
+                    Have a problem with your pet?<br>
+                    Call us now: 
+                    <span class="phone">
+                        1-800-123-1234
+                    </span>
+                </div>
+            </div>
+            <nav>
+                <ul class="sf-menu">
+                    <li class="current"><a href="home.html">main</a></li>
+                    <li><a href="about.html">about</a></li>
+                    <li>
+                        <a href="services.html">services</a>
+                        <ul>
+                            <li><a href="#">Maecenas faucibus</a></li>
+                            <li><a href="#">Fusce tincidunt</a></li>
+                            <li>
+                                <a href="#">tempor eros</a>
+                                <ul>
+                                    <li><a href="#">ut viverra</a></li>
+                                    <li><a href="#">hendrerit mauris</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">mauris ut du</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="links.html">links</a></li>
+                    <li><a href="contacts.html">contacts</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="clear"></div>
+    </div>
+
